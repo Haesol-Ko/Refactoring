464b46d1b0fa2192e47144960aa16a9c3d5a5280
Haesol <haesol0828@douzone.com>
2022-08-30 18:01:32 +0900

    step refactoring

--- ch6/6-11.js
+++ ch6/6-11.js
@@ -1,19 +1,29 @@
 export function priceOrder(product, quantity, shippingMethod) {
-  const basePrice = product.basePrice * quantity;
-  const discount =
-    Math.max(quantity - product.discountThreshold, 0) *
-    product.basePrice *
-    product.discountRate;
-  const shippingPerCase =
-    basePrice > shippingMethod.discountThreshold
-      ? shippingMethod.discountedFee
-      : shippingMethod.feePerCase;
-  const shippingCost = quantity * shippingPerCase;
-  const price = basePrice - discount + shippingCost;
-  return price;
+  const basePrice = calculateBasePrice(product,quantity);
+  const discount = calculateDiscount(product, quantity);
+  const shippingCost = calculateShippingCost(basePrice, quantity, shippingMethod);
+  return basePrice - discount + shippingCost;
 }
 
-// 사용 예:
+function calculateBasePrice(product, quantity) {
+  return product.basePrice * quantity;
+}
+
+function calculateDiscount(product, quantity) {
+  return Math.max(quantity - product.discountThreshold, 0) *
+      product.basePrice *
+      product.discountRate;
+}
+
+function calculateShippingCost(basePrice, quantity, shippingMethod) {
+  const shippingPerCase =
+      basePrice > shippingMethod.discountThreshold
+          ? shippingMethod.discountedFee
+          : shippingMethod.feePerCase;
+
+  return quantity * shippingPerCase;
+}
+
 const product = {
   basePrice: 10,
   discountRate: 0.1,
