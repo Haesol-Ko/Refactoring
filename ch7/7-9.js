f200ec9cb52b43598d9b29197fc3dc4a5c85acc5
Haesol <haesol0828@douzone.com>
2022-09-13 18:00:02 +0900

    change algorithms

--- ch7/7-9.js
+++ ch7/7-9.js
@@ -1,16 +1,11 @@
+/**
+ * 알고리즘 교체하기
+ * 파이프라인 이용
+ */
+
 function foundPerson(people) {
-  for (let i = 0; i < people.length; i++) {
-    if (people[i] === 'Don') {
-      return 'Don';
-    }
-    if (people[i] === 'John') {
-      return 'John';
-    }
-    if (people[i] === 'Kent') {
-      return 'Kent';
-    }
-  }
-  return '';
+  const candidates = ['Don', 'John', 'Kent'];
+  return people.find(p => candidates.includes(p)) || '';
 }
 
 console.log(foundPerson(['John']));
