d2f814658c7753c3f3f5572592f48836333db871
Haesol <haesol0828@douzone.com>
2022-09-13 17:31:27 +0900

    inline class

--- ch7/7-5.js
+++ ch7/7-5.js
@@ -1,66 +1,34 @@
-class TelephoneNumber {
-  #areaCode;
-  #number;
-
-  constructor(areaCode, number) {
-    this.#areaCode = areaCode;
-    this.#number = number;
-  }
-  get areaCode() {
-    return this.#areaCode;
-  }
-  set areaCode(arg) {
-    this.#areaCode = arg;
+export class Shipment {
+  #shippingCompany;
+  #trackingNumber;
+  constructor(trackingNumber, shippingCompany) {
+    this.#trackingNumber = trackingNumber;
+    this.#shippingCompany = shippingCompany;
   }
 
-  get number() {
-    return this.#number;
+  get trackingInfo() {
+    return `${this.shippingCompany}: ${this.trackingNumber}`;
   }
 
-  set number(arg) {
-    this.#number = arg;
+  get trackingNumber() {
+    return this.#trackingNumber;
   }
 
-  get toString() {
-    return `(${this.areaCode}) ${this.number}`;
+  set trackingNumber(arg) {
+    this.#trackingNumber = arg;
+  }
+
+  get shippingCompany() {
+    return this.#shippingCompany;
+  }
+
+  set shippingCompany(arg) {
+    this.#shippingCompany = arg;
   }
 }
 
-class Person {
-  #name;
-  #telephoneNumber;
-  constructor(name, areaCode, number) {
-    this.#name = name;
-    this.#telephoneNumber = new TelephoneNumber(areaCode, number);
-  }
+const shipment = new Shipment(999, 'Maersk');
+console.log(shipment.trackingInfo);
 
-  get name() {
-    return this.#name;
-  }
-
-  set name(arg) {
-    this.#name = arg;
-  }
-
-  /**
-   * person 내부에서 telephoneNumber에 접근해서 값 리턴하기. => getter가 두개
-   * @returns {() => string}
-   */
-  get telephoneNumber() {
-    return this.#telephoneNumber.toString;
-  }
-
-  get officeAreaCode() {
-    return this.#telephoneNumber.areaCode;
-  }
-
-  get officeNumber() {
-    return this.#telephoneNumber.number;
-  }
-}
-
-const person = new Person('엘리', '010', '12345678');
-console.log(person.name);
-console.log(person.officeAreaCode);
-console.log(person.officeNumber);
-console.log(person.telephoneNumber);
+shipment.shippingCompany = 'COSCO';
+console.log(shipment.trackingInfo);
