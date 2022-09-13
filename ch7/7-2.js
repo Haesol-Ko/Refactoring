b9da61905ab43e2414e6eb1fad484e0b02e37ca4
Haesol <haesol0828@douzone.com>
2022-09-13 14:28:27 +0900

    encapsulate collection

--- ch7/7-2.js
+++ ch7/7-2.js
@@ -10,12 +10,36 @@ export class Person {
     return this.#name;
   }
 
+  /**
+   * 새로운 객체로 만들어서 넘겨주기 => 불변성 유지
+   * @returns {*[]}
+   */
   get courses() {
-    return this.#courses;
+    return [...this.#courses];
   }
 
-  set courses(courses) {
-    this.#courses = courses;
+  /**
+   * 코스 추가 함수 만들기
+   * @param course
+   */
+  addCourse(course) {
+    this.#courses.push(course);
+  }
+
+  /**
+   * 코스 제거 함수 만들기
+   * 배열에서 원하는 객체 찾는 법 꼭 기억!!! indexOf, just return or splice
+   * runIfAbsent처럼 콜백함수를 받아와도 됨.
+   * @param course
+   * @param runIfAbsent
+   */
+  removeCourse(course, runIfAbsent) {
+    const index = this.#courses.indexOf(course);
+    if(index === -1) {
+      runIfAbsent();
+      return;
+    }
+    this.#courses.splice(index, 1);
   }
 }
 
@@ -37,5 +61,10 @@ export class Course {
 }
 
 const ellie = new Person('엘리');
-ellie.courses.push(new Course('리팩토링', true));
+const course = new Course('리팩토링', true);
+ellie.addCourse(course);
 console.log(ellie.courses.length);
+ellie.removeCourse(course, () => {console.log("해당 코스는 없다.")});
+console.log(ellie.courses.length);
+ellie.removeCourse(course, () => {console.log("해당 코스는 없다.")});
+
