f83289c1c44e2b11b063696a83c2c29fd23144b6
Haesol <haesol0828@douzone.com>
2022-09-15 14:56:10 +0900

    move line

--- ch8/8-4.js
+++ ch8/8-4.js
@@ -1,7 +1,12 @@
+/**
+ * 문장을 호출한 곳으로 옮기기
+ * 두가지 다른곳에서 로케이션을 출력하는 방식이 달라진걸 내가 어케알아요??
+ */
 function renderPerson(outStream, person) {
   outStream.write(`<p>${person.name}</p>\n`);
   renderPhoto(outStream, person.photo);
   emitPhotoData(outStream, person.photo);
+    outStream.write(`<p>location: ${person.photo.location}</p>\n`);
 }
 
 function listRecentPhotos(outStream, photos) {
@@ -10,14 +15,14 @@ function listRecentPhotos(outStream, photos) {
     .forEach((p) => {
       outStream.write('<div>\n');
       emitPhotoData(outStream, p);
-      outStream.write('</div>\n');
+        outStream.write(`<p>위치: ${photo.location}</p>\n`);
+        outStream.write('</div>\n');
     });
 }
 
 function emitPhotoData(outStream, photo) {
   outStream.write(`<p>title: ${photo.title}</p>\n`);
   outStream.write(`<p>date: ${photo.date.toDateString()}</p>\n`);
-  outStream.write(`<p>location: ${photo.location}</p>\n`);
 }
 
 function renderPhoto(outStream, aPhoto) {
