db95f92aa4fd0be0f660f1dc7d39dac87ff941d0
Haesol <haesol0828@douzone.com>
2022-09-15 16:22:23 +0900

    move some codes and finally pipelining which is amazing skill

--- ch8/8-5.js
+++ ch8/8-5.js
@@ -1,4 +1,9 @@
-let appliesToMass = false;
-for (const s of states) {
-  if (s === 'MA') appliesToMass = true;
-}
+/**
+ * 인라인 코드를 함수 호출로 바꾸기
+ */
+// let appliesToMass = false;
+// for (const s of states) {
+//   if (s === 'MA') appliesToMass = true;
+// }
+
+appliesToMass = states.include('MA');
