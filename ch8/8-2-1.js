278b88f12e49854868ab871a536d2eae3f140115
Haesol <haesol0828@douzone.com>
2022-09-15 12:01:43 +0900

    move field considering cohesion

--- ch8/8-2-1.js
+++ ch8/8-2-1.js
@@ -1,24 +1,22 @@
 export class Customer {
   #name;
-  #discountRate;
   #contract;
   constructor(name, discountRate) {
     this.#name = name;
-    this.#discountRate = discountRate;
     this.#contract = new CustomerContract(this.dateToday());
   }
 
   get discountRate() {
-    return this.#discountRate;
+    return this.#contract.discountRate;
   }
 
   becomePreferred() {
-    this.#discountRate += 0.03;
+    this.#contract.discountRate += 0.03;
     // 다른 코드들이 있음...
   }
 
   applyDiscount(amount) {
-    return amount.subtract(amount.multiply(this.#discountRate));
+    return amount.subtract(amount.multiply(this.discountRate));
   }
 
   dateToday() {
@@ -28,7 +26,16 @@ export class Customer {
 
 class CustomerContract {
   #startDate;
-  constructor(startDate) {
+  #discountRate;
+
+  constructor(startDate, discountRate) {
     this.#startDate = startDate;
+    this.#discountRate = discountRate;
+  }
+  get discountRate() {
+    return this.#discountRate;
+  }
+  set discountRate(value) {
+    this.#discountRate = value;
   }
 }
