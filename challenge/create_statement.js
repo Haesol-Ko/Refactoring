fb14308783cccf2d8b4fceb8f79a65fe1c3f9658
Haesol <haesol0828@douzone.com>
2022-09-29 23:32:13 +0900

    use polymorphism

--- challenge/create_statement.js
+++ challenge/create_statement.js
@@ -10,47 +10,56 @@ class Performance {
         return this.#audience;
     }
 
-    get amount() {
-        let result = 0;
-        switch (this.#play.type) {
+    get play() {
+        return this.#play;
+    }
+
+    // 생성할 때 쉽도록 팩터리함수 만들기. 여기서는 switch 써도 됨
+    static create(audience, play) {
+        switch (play.type) {
             case 'tragedy':
-                result = 40000;
-                if (this.#audience > 30) {
-                    result += 1000 * (this.#audience - 30);
-                }
-                break;
+                return new Tragedy(audience, play);
             case 'comedy':
-                result = 30000;
-                if (this.#audience > 20) {
-                    result += 10000 + 500 * (this.#audience - 20);
-                }
-                result += 300 * this.#audience;
-                break;
+                return new Comedy(audience, play);
+                // null객체를 만들던 오류를 던지던 선택!
             default:
-                throw new Error(`알 수 없는 장르: ${this.#play.type}`);
+                throw new Error(`알 수 없는 타입!: ${play.type}`);
         }
-        return result;
+    }
+}
+
+class Tragedy extends Performance {
+    get amount() {
+        const base = 40000;
+        return this.audience > 30 ? base + 1000 * (this.audience - 30) : base;
     }
 
     get credit() {
-        let result = 0;
-        result = Math.max(this.#audience - 30, 0);
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ('comedy' === this.#play.type) {
-            result += Math.floor(this.#audience / 5);
+        return Math.max(this.audience - 30, 0);
+    }
+}
+
+class Comedy extends Performance {
+    get amount() {
+        let result = 30000;
+        if (this.audience > 20) {
+            result += 10000 + 500 * (this.audience - 20);
         }
+        result += 300 * this.audience;
+
         return result;
     }
 
-    get play() {
-        return this.#play;
+    // 뭐가 뭘 의미하는지 따로 변수를 추출해서 뭘 의미하는지 알려주면 더 좋다!
+    get credit() {
+        return Math.max(this.audience - 30, 0) + Math.floor(this.audience / 5);
     }
 }
 
 export function createStatement(invoice, plays) {
     const statement = {};
     statement.customer = invoice.customer;
-    statement.performances = invoice.performances.map(p => new Performance(p.audience, plays[p.playID])); // 전달하는 인자와 호출하는 변수가 같다면 생략 가능.. (p=>enrich(p)) === (enrich)
+    statement.performances = invoice.performances.map(p => Performance.create(p.audience, plays[p.playID])); // 전달하는 인자와 호출하는 변수가 같다면 생략 가능.. (p=>enrich(p)) === (enrich)
     statement.totalAmount = totalAmount(statement.performances);
     statement.totalCredits = totalCredits(statement.performances);
     return statement;
