f2fe211b692fb4396fbebdff844597ab791ddb70
Haesol <haesol0828@douzone.com>
2022-09-29 23:15:04 +0900

    make function to class

--- challenge/create_statement.js
+++ challenge/create_statement.js
@@ -1,54 +1,60 @@
+class Performance {
+    #audience;
+    #play;
+    constructor(audience, play) {
+        this.#audience = audience;
+        this.#play = play;
+    }
+
+    get audience() {
+        return this.#audience;
+    }
+
+    get amount() {
+        let result = 0;
+        switch (this.#play.type) {
+            case 'tragedy':
+                result = 40000;
+                if (this.#audience > 30) {
+                    result += 1000 * (this.#audience - 30);
+                }
+                break;
+            case 'comedy':
+                result = 30000;
+                if (this.#audience > 20) {
+                    result += 10000 + 500 * (this.#audience - 20);
+                }
+                result += 300 * this.#audience;
+                break;
+            default:
+                throw new Error(`알 수 없는 장르: ${this.#play.type}`);
+        }
+        return result;
+    }
+
+    get credit() {
+        let result = 0;
+        result = Math.max(this.#audience - 30, 0);
+        // 희극 관객 5명마다 추가 포인트를 제공한다.
+        if ('comedy' === this.#play.type) {
+            result += Math.floor(this.#audience / 5);
+        }
+        return result;
+    }
+
+    get play() {
+        return this.#play;
+    }
+}
+
 export function createStatement(invoice, plays) {
     const statement = {};
     statement.customer = invoice.customer;
-    statement.performances = invoice.performances.map(enrichPerformance); // 전달하는 인자와 호출하는 변수가 같다면 생략 가능.. (p=>enrich(p)) === (enrich)
+    statement.performances = invoice.performances.map(p => new Performance(p.audience, plays[p.playID])); // 전달하는 인자와 호출하는 변수가 같다면 생략 가능.. (p=>enrich(p)) === (enrich)
     statement.totalAmount = totalAmount(statement.performances);
     statement.totalCredits = totalCredits(statement.performances);
     return statement;
 
-    function enrichPerformance(performance){
-        const result = {...performance};
-        result.play = playFor(performance);
-        result.amount = amountFor(result);
-        result.credit = creditsFor(result);
-        return result;
-
-        function playFor(performance) {
-            return plays[performance.playID];
-        }
-
-        function amountFor(performance) {
-            let result = 0;
-            switch (performance.play.type) {
-                case 'tragedy':
-                    result = 40000;
-                    if (performance.audience > 30) {
-                        result += 1000 * (performance.audience - 30);
-                    }
-                    break;
-                case 'comedy':
-                    result = 30000;
-                    if (performance.audience > 20) {
-                        result += 10000 + 500 * (performance.audience - 20);
-                    }
-                    result += 300 * performance.audience;
-                    break;
-                default:
-                    throw new Error(`알 수 없는 장르: ${performance.play.type}`);
-            }
-            return result;
-        }
-    }
-
-    function creditsFor(performance) {
-        let result = 0;
-        result = Math.max(performance.audience - 30, 0);
-        // 희극 관객 5명마다 추가 포인트를 제공한다.
-        if ('comedy' === performance.play.type) {
-            result += Math.floor(performance.audience / 5);
-        }
-        return result;
-    }
     function totalCredits(performances) {
         return performances.reduce(
             (sum, p) => (sum += p.credit)
